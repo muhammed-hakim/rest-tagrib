b905ebf3919d2897a5798f22576924835a722a89
Muhammed Hakim <121636112+muhammed-hakim@users.noreply.github.com>
2026-04-02 14:02:00 +0300

    Delete gourmet-backup directory

--- gourmet-backup/src/app/(customer)/product/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { notFound } from "next/navigation"
-import { getDishById } from "@/sanity/queries"
-import ProductClient from "./ProductClient"
-
-export default async function ProductPage({ params }: { params: { id: string } }) {
-  const dish = await getDishById(params.id)
-  if (!dish) return notFound()
-  return <ProductClient dish={dish} />
-}
